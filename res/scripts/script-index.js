511669111e3e7ca9298a4e3b3d5dc990d13e1477
John <49390743+techryon@users.noreply.github.com>
2019-05-06 23:42:59 +1200

    Reset & Card flips
    Cards will be flipped only once
    Reset button now reloads the page
    It enables the card flips to work again

--- res/scripts/script-index.js
+++ res/scripts/script-index.js
@@ -1,24 +1,55 @@
+
+//Creating a new array to store the face images in the deck of card
+var faceCardsArray = new Array(); 
+
+//Creating Two random values to use while switching cards
+var randomOne = Math.floor((Math.random() * 52) + 0);
+var randomTwo = Math.floor((Math.random() * 52) + 0);
+
 //AddEventListener to DOMContentLoaded, so the image waits to load
 //after the page is loaded
+
 document.addEventListener("DOMContentLoaded", function(event) {
 	onload_image();
-  
+
+  //The array has been created so its available in the memory during the game
+  for (var faceCard=0; faceCard<=51; faceCard++){
+    faceCardsArray[faceCard] = new Image(); //Every element of the array is defined as a new image
+    faceCardsArray[faceCard].src = "./res/images/face/" + faceCard + ".png"; //every element source has been set
+
+  }
+ 
 });
 
-var cardOne = document.getElementById("cardOne");
-cardOne.onclick = function() {
-  changeImage(cardOne);
+
+//Capturing which card has been Clicked
+//And it's id has been passed on 
+var cardOne = document.getElementById("displayingImage1");
+cardOne.onclick = function(alt) {
+  changeImage(cardOne, randomOne);
+  cardOne.alt = "true";
+  this.onclick = null;
+  return true
 }
 
-var cardTwo = document.getElementById("cardTwo");
-cardTwo.onclick = function() {
-  changeImage(cardTwo);
+
+var cardTwo = document.getElementById("displayingImage2");
+cardTwo.onclick = function(alt) {
+  changeImage(cardTwo, randomTwo);
+  cardTwo.alt = "true";
+  this.onclick = null;
+  return true;
 }
 
+if(cardOne.alt == true){
+  console.log("test");
+}
 
+//Reset button to reset the images back to start
 var reset = document.getElementById("reset");
 reset.onclick = function() {
-  onload_image();
+  window.location.reload();
+
 }
 
 //Function to load both the default images. Looping it so it loads the same image twice
@@ -33,15 +64,17 @@ function onload_image() {
 }
 
 
-
-
-
 //Function to flip the card and display the changed image.
-//This function check which parent tag is clicked and change image on its child
-function changeImage(parent){
-  var src = "./res/images/front-Spade-A.png";
-  var img = parent.children[0];
-  img.src = src;
+//This function takes id of the img and set the source attribute with changed image
+var src="";
+function changeImage(idimage, randomValue){
+  //var randomElement = Math.floor((Math.random() * 52) + 0);
+  src = faceCardsArray[randomValue].src;
+  idimage.setAttribute("src", src);
+  console.log(src);
 
 }
+
+
+
 
